df0c8b5acbe07f11a8a7429f48dae4780f744381
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-05-23 13:57:47 -0300

    Taking out console.log

--- src/components/colorPicker/index.jsx
+++ src/components/colorPicker/index.jsx
@@ -6,6 +6,5 @@ export const ColorPickerComponent = () => {
     const theme = useContext(ThemeContext);
     const bubbles = Object.entries(theme.bgColors).filter(([key]) => selected ? selected === key : true).map(([key, value]) =>
         <ColorBubble key={`bubble_color_${key}`} bgColor={value} onClick={key === selected ? () => setSelected(null) : () => setSelected(key)} />)
-        console.log(bubbles);
     return <BubblesContainer>{bubbles}</BubblesContainer>
 }
